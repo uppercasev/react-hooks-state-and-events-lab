e57c9741c9b6f689142b0188e9fc51fcb4e8e60c
Vi Tran <contact.vitran@gmail.com>
2021-12-08 20:52:01 +1100

    Dark mode, Add to cart finished

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,19 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
-
+  const [darkModeOn, setDarkModeOn] = useState(false);
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
+  function darkModeFunction() {
+    setDarkModeOn((darkModeOn) => !darkModeOn);
+  }
+
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (darkModeOn ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={darkModeFunction}>{darkModeOn ? "Light Mode" : "Dark Mode"}</button>
       </header>
       <ShoppingList items={items} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+  
+  function inCartFunction() {
+    setIsInCart((isInCart) => !isInCart);
+  }
+
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={inCartFunction}>Add to Cart</button>
     </li>
   );
 }
